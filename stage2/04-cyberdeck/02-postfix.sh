215e2ddc266b44b8874b6ba2d13abe26d25042db
Your Name <you@example.com>
2023-06-25 18:54:35 +0200

    cleanup

--- stage2/04-cyberdeck/02-postfix.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-debconf-set-selections <<< "postfix postfix/mailname string cyberjutsu.local"
-debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
-apt-get install --assume-yes postfix
